1d55fafe0237ee39cc5fe83e01902f7d3ca0c6af
Amanda Estephany <158222303+amandaestephany@users.noreply.github.com>
2025-12-18 19:56:34 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import os 
+
+def exibir_nome_programa():
+    print("""
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+""")
+
+def exibir_opcoes():
+    print("1. Cadastrar restaurante.")
+    print("2. Listar restaurante.")
+    print("3. Ativar restaurante.")
+    print("4. Sair.\n")
+
+
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o app.\n")
+
+def opcao_invalida():
+    print("Opção Inválida\n")
+    input("Digite uma tecla para voltar para o menu principal")
+    main()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: \n"))
+
+        if opcao_escolhida == 1:
+            print("Cadastrar restaurantes.")
+        elif opcao_escolhida == 2:
+            print("Listar restaurantes.")
+        elif opcao_escolhida == 3:
+            print("Ativar restaurante.")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system("cls")
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
